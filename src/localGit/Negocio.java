f28a7ca0881fbf13efb870d6c5d4cd03478e3c73
reysoac <rromeroprogramacion@gmail.com>
2021-03-03 12:53:56 -0600

    creacion metodo consultaLocal()

--- src/localGit/Negocio.java
+++ src/localGit/Negocio.java
@@ -5,6 +5,9 @@ public class Negocio {
 	public void consulta() {
 		System.out.println("Negocio.consulta() Comentario local 1");
 	}
+	public void consultaLocal() {
+		System.out.println("Negocio.consultaLocal() Comentario local 1");
+	}
 	
 
 }
